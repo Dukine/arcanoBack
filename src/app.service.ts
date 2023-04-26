ccfc676c715a1539503e478ba0df2f17056e082c
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-26 19:28:28 -0300

    Created Request methods

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Observable, firstValueFrom } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  getUserCart() {
-    return 'History';
+  async getUserHistory() {
+    return await this.getUserCart('2');
+  }
+
+  //Http Requests
+  async getProducts() {
+    const { data } = await firstValueFrom(
+      this.httpService.get('https://fakestoreapi.com/products'),
+    );
+
+    return data;
+  }
+
+  async getUser(id: string) {
+    const { data } = await firstValueFrom(
+      this.httpService.get(`https://fakestoreapi.com/users/${id}`),
+    );
+
+    return data;
+  }
+
+  //FakeAPI error without a date.
+  async getUserCart(id: string) {
+    const { data } = await firstValueFrom(
+      this.httpService.get(
+        `https://fakestoreapi.com/carts/user/${id}?startdate=2019-01-01&enddate=2023-12-31`,
+      ),
+    );
+
+    return data;
   }
 }
