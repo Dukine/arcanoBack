d48f383f041b41b30bea326ae94c44c03361b97a
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-27 19:32:21 -0300

    Http requests finished, and types make

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { Observable, firstValueFrom } from 'rxjs';
 import { AxiosResponse } from 'axios';
+import { iCart, iProduct, iUser } from './interfaces';
 
 @Injectable()
 export class AppService {
@@ -16,27 +17,31 @@ export class AppService {
   }
 
   //Http Requests
-  async getProducts() {
+  async getProducts(): Promise<iProduct[]> {
     const { data } = await firstValueFrom(
-      this.httpService.get('https://fakestoreapi.com/products'),
+      this.httpService.get<iProduct[]>('https://fakestoreapi.com/products'),
     );
 
     return data;
   }
 
-  async getUser(id: string) {
+  async getUser(id: string): Promise<iUser> {
     const { data } = await firstValueFrom(
-      this.httpService.get(`https://fakestoreapi.com/users/${id}`),
+      this.httpService.get<iUser>(`https://fakestoreapi.com/users/${id}`),
     );
 
     return data;
   }
 
   //FakeAPI error without a date.
-  async getUserCart(id: string) {
+  async getUserCart(id: string): Promise<iCart[]> {
+    const todayDate = new Date();
+
     const { data } = await firstValueFrom(
-      this.httpService.get(
-        `https://fakestoreapi.com/carts/user/${id}?startdate=2019-01-01&enddate=2023-12-31`,
+      this.httpService.get<iCart[]>(
+        `https://fakestoreapi.com/carts/user/${id}?startdate=2019-01-01&enddate=${
+          todayDate.toISOString().split(`T`)[0]
+        }`,
       ),
     );
 
