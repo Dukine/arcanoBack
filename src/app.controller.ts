bd762d793136f3e8fc64f69d6269ab219a924913
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-29 15:25:59 -0300

    feat: UserHistory serialization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  SerializeOptions,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +12,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('cart-history')
+  @SerializeOptions({ excludeExtraneousValues: true })
+  @UseInterceptors(ClassSerializerInterceptor)
   getUserCart() {
     return this.appService.getUserHistory('2');
   }
